6450632b1462b7e7ec221dd8820c73309479f192
Marco Franssen <marco.franssen@gmail.com>
2022-08-02 17:09:21 +0200

    Implement bootstrapping empty provider

---
+++ pkg/curl/provider.go
@@ -0,0 +1,32 @@
+package curl
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func NewProvider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"token": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("CURL_OAUTH2_TOKEN", nil),
+			},
+		},
+		ResourcesMap:         map[string]*schema.Resource{},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	_ = d.Get("token").(string)
+
+	var diags diag.Diagnostics
+
+	return nil, diags
+}
